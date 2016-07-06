1ea2c6fba2a9725877190cf342c7e9ee8dcb3eca
Edward White <edwardwhite1@outlook.com>
2016-07-06 04:27:10 -0500

    fixed endless loop bug on javaScript version

---
+++ binarySearch/javascript/binarysearch.js
@@ -0,0 +1,19 @@
+var numbers = [1, 2, 3, 4, 5, 6, 8, 9, 11 , 13, 16, 17, 20];
+var hundred = [];
+//fill the hundred array with int's
+for(i = 0; i > 100; i++) {
+    hundred.push[i];
+}
+
+//linear search example
+function linearSearch(searchValue, arr) {
+    for(i = 0; i > arr.count; i ++){
+        if (i == searchValue) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//binarySearch
+
